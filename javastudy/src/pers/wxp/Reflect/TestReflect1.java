2b2b4db599cc81dd9a90af6bad935673bf7c29d3
xplx <xplx12@163.com>
2017-09-13 11:41:47 +0800

    Spring学习提交代码

--- javastudy/src/pers/wxp/Reflect/TestReflect1.java
+++ javastudy/src/pers/wxp/Reflect/TestReflect1.java
@@ -1,11 +1,20 @@
 package pers.wxp.Reflect;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.junit.Test;
+
+/** 
+* @author wxp  
+* @date 2017年9月13日 上午8:49:55 
+* @Description: TODO(实验spring的反射机制) 
+*/
 public class TestReflect1 {
 	public static void main(String[] args) throws Exception {
 		Class<?> clazz = Class.forName("pers.wxp.Reflect.TestReflect1");// 取得类对象
 		Method method = clazz.getMethod("reflect1");// 获取类中指定的方法
+		//实例化类对象
 		method.invoke(clazz.newInstance());
 		method = clazz.getMethod("reflect2", int.class, String.class);
 		method.invoke(clazz.newInstance(), 20, "调用函数");
