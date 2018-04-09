a0af3ba5c2aceb8089a83006c6293cd517a11f82
wuxiaopeng <1099272020@qq.com>
2018-04-09 15:54:16 +0800

    本地代码提交

--- javastudy/src/pers/wxp/type/number/LongDemo.java
+++ javastudy/src/pers/wxp/type/number/LongDemo.java
@@ -2,28 +2,38 @@ package pers.wxp.type.number;
 
 import org.junit.Test;
 
-/** 
-* @author wxp  
-* @date 2018年1月29日 下午1:35:46 
-* @Description: TODO(这里用一句话描述这个类的作用) 
-*/
+/**
+ * @author wxp
+ * @date 2018年1月29日 下午1:35:46
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ */
 public class LongDemo {
-	@Test
-	public void LongValueOf() {
-		String str = "C89ED7DE";
-		System.out.println(str.toLowerCase());
-		String str1 = "00000000" + "C89ED7DE";
-		Long len = Long.valueOf(str, 16);
-		Long len1 = Long.valueOf(str1, 16);
-		System.out.println(len);		
-		System.out.println(len1);		
-		String strhex = Long.toHexString(len);
-		System.out.println(strhex);
-	}
-	
-	@Test
-	public void floatCompare(){
-		//3.4是双精度数据（double）
-		float f1 = 3;
-	}
+    @Test
+    public void LongValueOf() {
+        String str = "C89ED7DE";
+        System.out.println(str.toLowerCase());
+        String str1 = "00000000" + "C89ED7DE";
+        Long len = Long.valueOf(str, 16);
+        Long len1 = Long.valueOf(str1, 16);
+        System.out.println(len);
+        System.out.println(len1);
+        String strhex = Long.toHexString(len);
+        System.out.println(strhex);
+    }
+
+
+    @Test
+    public void floatCompare() {
+        //3.4是双精度数据（double）
+        float f1 = 3;
+    }
+
+    /**
+     * Float类和Double类的intValue方法，就是丢掉了小数位
+     */
+    @Test
+    public void intValueTest() {
+        Double a = 3.3;
+        System.out.println(a.intValue());
+    }
 }
