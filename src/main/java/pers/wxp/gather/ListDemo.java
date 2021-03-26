e80eed7d9edef39928081cc6c7e0c1cb50c5df52
mycomputer <xplx12@163.com>
2021-03-26 10:09:02 +0800

    线程池修改

--- src/main/java/pers/wxp/gather/ListDemo.java
+++ src/main/java/pers/wxp/gather/ListDemo.java
@@ -2,28 +2,19 @@ package pers.wxp.gather;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
 
 public class ListDemo {
 
-	/**
-	 * 不要在 foreach 循环里进行元素的 remove/add 操作。 remove 元素请使用 Iterator方式，
-	 */
 	@Test
 	public void removeDemo() {
 		List<String> a = new ArrayList<String>();
 		a.add("1");
 		a.add("2");
 
-//		for (String temp : a) {
-//			if ("2".equals(temp)) {// 这里使用equals时，使用已知的值去相等未知的值，免得出现错误。
-//				a.remove(temp);
-//			}
-//		}
-//		System.out.println(a);
-		
 		Iterator<String> it = a.iterator();
 		while (it.hasNext()) {
 			String temp = it.next();
@@ -32,6 +23,7 @@ public class ListDemo {
 			}
 		}
 		System.out.println(a);
+		List l = new ArrayList<>();
+		List ll = new LinkedList();
 	}
-
 }
--- src/main/java/pers/wxp/threadpool/ExecutorServiceDemo.java
+++ src/main/java/pers/wxp/threadpool/ExecutorServiceDemo.java
@@ -9,10 +9,20 @@ import java.util.concurrent.*;
  */
 public class ExecutorServiceDemo {
     //手动创建线程
+    /**
+     * workQueue:当新任务来的时候会先判断当前运行的线程数量是否达到核心线程数
+     * ，如果达到的话，任务就会被存放在队列中。
+     * threadFactory：为线程池提供创建新线程的线程工厂。
+     * handler：线程池任务队列超过 maxinumPoolSize 之后的拒绝策略。
+     * (1)AbortPolicy:抛出 RejectedExecutionException来拒绝新任务的处理。
+     * (2)CallerRunsPolicy:调用执行自己的线程运行任务,影响程序的整体性能。任务不会丢失。
+     *（3）DiscardPolicy：不处理新任务，直接丢弃掉。
+     *（4）DiscardOldestPolicy： 此策略将丢弃最早的未处理的任务请求。
+     */
     private ExecutorService pool = new ThreadPoolExecutor(3, 10,
             10L, TimeUnit.SECONDS,
             new LinkedBlockingQueue<Runnable>(512), Executors.defaultThreadFactory(),
-            new ThreadPoolExecutor.AbortPolicy());
+            new ThreadPoolExecutor.CallerRunsPolicy());
     public static ExecutorService executorService = Executors.newFixedThreadPool(10);
     /**
      * Executors类创建线程池的时候实际就是调用ThreadPoolExecutor类的构造方法来创建,
