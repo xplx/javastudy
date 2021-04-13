3ad40b145f955de910a54d712edb1aa08e86ad82
mycomputer <xplx12@163.com>
2021-04-13 15:02:33 +0800

    java学习资料

---
+++ src/main/java/pers/wxp/type/StrackTest.java
@@ -0,0 +1,16 @@
+package pers.wxp.type;
+
+/**
+ * @author xiaopeng
+ * @date 2021年04月02日 10:39
+ * @description
+ */
+public class StrackTest {
+    public static void main(String[] args) {
+        eat();
+    }
+
+    public static void  eat(){
+        eat();
+    }
+}
